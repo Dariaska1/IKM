7fe51b62807d6b1740bb476fe689c5911586a865
Dariaska1 <Charli604052@gmail.com>
2025-06-25 10:46:49 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,14 +1,9 @@
 #pragma once
-#ifndef FACTORIAL_H
-#define FACTORIAL_H
+#include <iostream>
+using namespace std;
 
-#include <vector>
-#include <string>
+// Вычисление факториала числа n (n!)
+long double factorial(unsigned int n);
 
-// Âû÷èñëåíèå ôàêòîðèàëà ÷èñëà n (n!)
-unsigned long long factorial(unsigned int n);
-
-// Âû÷èñëåíèå ôàêòîðèàëà îò ôàêòîðèàëà (n!)!
-unsigned long long double_factorial(unsigned int n);
-
-#endif // FACTORIAL_H
+// Вычисление факториала от факториала (n!)!
+long double double_factorial(unsigned int n);
